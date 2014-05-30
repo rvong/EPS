984bb6619be9156aff3656016e16e66c9c67b04b
Richard Vong <rich.vong@hotmail.com>
2014-05-29 21:31:46 -0700

    saved 1 char; spaces

--- CodeGolf.rb
+++ CodeGolf.rb
@@ -1,4 +1,4 @@
-fq = Hash.new(0)
-s = File.read('../stop_words.txt').split(',')
-File.read(ARGV[0]).downcase.scan(/[a-z]{2,}/).reject{|w| s.include? w}.each{|w| fq[w]+=1}
-fq.sort_by{|x,y|-y}.take(25).each{|w,f| puts w+' - '+f.to_s} 
+h=Hash.new(0)
+s=File.read('../stop_words.txt').split(',')
+File.read(ARGV[0]).downcase.scan(/[a-z]{2,}/).reject{|w|s.include?w}.each{|w|h[w]+=1}
+h.sort_by{|x,y|-y}.take(25).each{|w,f|puts w+' - '+f.to_s}
