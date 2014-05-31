d2337df72a0feb503b6000389f8e9ef21f66275f
Richard Vong <rich.vong@hotmail.com>
2014-05-31 02:07:27 -0700

    0..24 vs take(25)

--- CodeGolf.rb
+++ CodeGolf.rb
@@ -1,4 +1,4 @@
 h=Hash.new(0)
 s=File.read('../stop_words.txt').split(',')
 File.read(ARGV[0]).downcase.scan(/[a-z]{2,}/).reject{|w|s.include?w}.each{|w|h[w]+=1}
-h.sort_by{|x,y|-y}.take(25).each{|w,f|puts w+' - '+f.to_s}
+h.sort_by{|x,y|-y}[0..24].each{|w,f|puts w+' - '+f.to_s}
